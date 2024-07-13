9e1ee242505bc5f734bbfb5f52e2028acf3e58e2
abubbakr0904 <abubbakroqilov03@gmail.com>
2024-07-13 18:00:06 +0300

    change images

--- lib/utils/images/app_images.dart
+++ lib/utils/images/app_images.dart
@@ -5,6 +5,10 @@ class AppImages{
   static const String rus = "assets/images/rus.png";
   static const String english = "assets/images/english.jpg";
   static const String loggo = "assets/images/loggo.jpg";
+  static const String dds = "assets/images/loggo.jpg";
+  static const String logdsdsgo = "assets/images/loggo.jpg";
+  static const String logasdago = "assets/images/loggo.jpg";
+  static const String werd = "assets/images/loggo.jpg";
 
   //svg
   static const String changeIcon = "assets/icons/change.svg";
